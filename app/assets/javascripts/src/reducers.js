c74482df7fa3d2a0153fd8a98c177698df445963
talum <tracy.a.lum@gmail.com>
2017-01-17 22:05:01 -0500

    add reducer

---
+++ app/assets/javascripts/src/reducers.js
@@ -0,0 +1,14 @@
+const initialState = {
+  iceCreams: []
+}
+
+const creameryApp = function creameryApp(state = initialState, action) {
+  switch (action.type) {
+    case 'show_all':
+      return state
+  }
+  default: 
+    return state
+} 
+
+export default creameryApp
