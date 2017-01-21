1b22d7f891d71c92b57ca0794533e3569aad97f6
talum <tracy.a.lum@gmail.com>
2017-01-21 12:05:56 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root to: "landing#index"
   namespace :api do
     namespace :v1 do
-      resources :ice_creams, only: :index
+      resources :ice_creams, only: [:index, :create, :update]
+      resources :comments, only: [:create, :update]
+      resources :parlors, only: [:index, :create, :update]
+      resources :profiles, only: [:create, :update, :show]
+      resources :reviews, only: [:create, :update, :index]
+      resources :users, only: :create
     end
   end
 end
