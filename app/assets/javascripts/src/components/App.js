d2a1ac6a8c2591e02b167e0ec4c3c83a0c37c367
talum <tracy.a.lum@gmail.com>
2017-01-21 21:31:36 -0500

    user index routes with router

--- app/assets/javascripts/src/components/App.js
+++ app/assets/javascripts/src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router'
-import IceCreamListContainer from '../components/iceCreams/IceCreamListContainer'
 
-const App = () => (
-  <div className="container">
-    <div>
-      <Link to='/users'>Sign Up</Link>
+const App = (props) => (
+  <div>
+    <nav>
+      <div className="nav-wrapper deep-purple darken-4">
+        <div className="brand-logo">Creamery</div>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><Link to='/users'>Sign Up</Link></li>
+        </ul>
+      </div>
+    </nav>
+    <div className="container">
+      { props.children || "hello"}
     </div>
-    <IceCreamListContainer />
   </div>
 )
 
--- app/assets/javascripts/src/index.js
+++ app/assets/javascripts/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, Link, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import App from './components/App'
 import NewUserForm from './components/users/NewUserForm'
+import IceCreamListContainer from './components/iceCreams/IceCreamListContainer'
 import store from './store'
 
 document.addEventListener("DOMContentLoaded", function () {
   ReactDOM.render(
     <Provider store={store}>
       <Router history={browserHistory} >
-        <Route path="/" component={App} />
-        <Route path="/users" component={NewUserForm} />
+        <Route path="/" component={App}>
+          <IndexRoute component={IceCreamListContainer} />
+          <Route path="/users" component={NewUserForm} />
+        </Route>
       </Router>
     </Provider>,
     document.getElementById('root')
