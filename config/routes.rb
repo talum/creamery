a5548e6831c82bdbc3d42f3eabd6fd5d474feee7
talum <tracy@flatironschool.com>
2017-02-23 10:21:09 -0500

    ignore the format

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,5 @@ Rails.application.routes.draw do
       post '/login', to: "sessions#create"
     end
   end
-  get '*path', to: "landing#index"
+  get '*path', to: "landing#index", format: false
 end
