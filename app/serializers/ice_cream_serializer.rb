2afc3282e5900ae24aaf83e45798ca67122662a8
talum <tracy.a.lum@gmail.com>
2017-03-14 12:27:11 -0400

    fix image bug

--- app/serializers/ice_cream_serializer.rb
+++ app/serializers/ice_cream_serializer.rb
@@ -1,8 +1,9 @@
 class IceCreamSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   attributes :id, :title, :parlor_id, :flavors, :ice_cream_flavors, :reviews, :review_ids, :image_url
 
   def image_url
-    object.image.url
+    root_url + object.image.url
   end
 
 end
