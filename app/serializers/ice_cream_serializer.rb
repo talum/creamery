75c960ad50af86fa22cecd639b2ae546d292c5c9
talum <tracy.a.lum@gmail.com>
2017-03-15 22:29:46 -0400

    enable image upload to s3

--- app/serializers/ice_cream_serializer.rb
+++ app/serializers/ice_cream_serializer.rb
@@ -1,9 +1,8 @@
 class IceCreamSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
   attributes :id, :title, :parlor_id, :flavors, :ice_cream_flavors, :reviews, :review_ids, :image_url
 
   def image_url
-    root_url + object.image.url
+    object.image.url
   end
 
 end
