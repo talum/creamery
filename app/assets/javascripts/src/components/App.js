177252c8b65d055c193f4a7a5c6cc06bd2d59e35
talum <tracy.a.lum@gmail.com>
2017-03-27 22:01:38 -0400

    add footer

--- app/assets/javascripts/src/components/App.js
+++ app/assets/javascripts/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from './Nav'
+import moment from 'moment'
 
 const App = (props) => (
   <div>
@@ -16,6 +17,17 @@ const App = (props) => (
       </div>
       <div className="site-layout__inner">
         { props.children }
+      </div>
+      <div className="site-footer">
+        <div className='site-footer__inner'>
+          <div className="flex-grid flex-grid--thirds">
+            <div className="flex-grid__item"/>
+            <div className="flex-grid__item">
+              <h3 className="heading heading--level-3 heading--color-blue util--text-align-c">Made and maintained by Tracy Lum &#8212; Copyright { moment().format('YYYY') }</h3>
+            </div>
+            <div className="flex-grid__item"/>
+          </div>
+        </div>
       </div>
     </div>
   </div>
