0f0f036654c3024c0b1c46d4f9deb33799af5d9a
talum <tracy.a.lum@gmail.com>
2017-04-15 14:50:06 -0400

    show parlor address

--- app/serializers/parlor_serializer.rb
+++ app/serializers/parlor_serializer.rb
@@ -1,5 +1,5 @@
 class ParlorSerializer < ActiveModel::Serializer
-  attributes :id, :name, :ice_creams
+  attributes :id, :name, :ice_creams, :street_address, :city, :state, :zip_code
 
   def ice_creams
     object.ice_creams.pluck(:id)
