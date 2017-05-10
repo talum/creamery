6f74f2449151a20feb5e39a7003d41c09b430a48
talum <tracy.a.lum@gmail.com>
2017-05-10 00:08:34 -0400

    rm validations

--- app/models/parlor.rb
+++ app/models/parlor.rb
@@ -3,8 +3,4 @@ class Parlor < ApplicationRecord
   has_many :favorites, as: :favoritable
 
   validates :name, presence: true
-  validates :street_address, presence: true
-  validates :city, presence: true
-  validates :state, presence: true
-  validates :zip_code, presence: true
 end
